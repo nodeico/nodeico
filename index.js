b41d379b00c548f42dc8d3520a7d56add3f356b7
Subhajit Sahu <wolfram77@gmail.com>
2018-03-16 21:23:29 +0530

    :banana: svg with fit width generated, url must end with .svg

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+const queryString = require('query-string');
 const httpStatus = require('http-status');
+const moment = require('moment');
 const https = require('https');
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
 const E = process.env;
-const CARD = fs.readFileSync('card.svg');
-const LOGO = fs.readFileSync('logo.svg');
+const CARD = fs.readFileSync('card.svg', 'utf8');
+const LOGO = fs.readFileSync('logo.svg', 'utf8');
 
 function request(pth, mth='GET') {
   console.log(`> ${mth} ${pth}`);
   return new Promise((fres, frej) => {
-    var pto = url.parse(pth);
-    var opt = {hostname: pto.hostname, port: pto.port, path: pto.path, method: mth};
-    var req = (pto.protocol==='https:'? https:http).request(opt, (res) => {
+    var opt = {hostname: 'unpkg.com', path: pth, method: mth};
+    var req = https.request(opt, (res) => {
       res.setEncoding('utf8');
       var z = '', cod = res.statusCode, sta = httpStatus[cod];
       console.log(`< ${mth} ${pth} : ${cod} ${sta}`);
@@ -29,31 +30,49 @@ function request(pth, mth='GET') {
   });
 };
 
-function pkg(nam) {
-  var opt = {hostname: 'unpkg.com', path: `/${nam}`};
-  var req = https.request(opt, (res) => {
-    
+function argument(qry) {
+  var width = parseFloat(qry.width)||384, height = parseFloat(qry.height)||56, margin = parseFloat(qry.margin)||4;
+  return {width, height, margin};
+};
+
+function package(nam) {
+  return request(`https://unpkg.com/${nam}/package.json`).then((res) => {
+    var pkg = JSON.parse(res.body), mod = res.headers['last-modified'];
+    var install = (pkg.preferGlobal? '-g ':'')+pkg.name, deps = Object.keys(pkg.dependencies||{}).length;
+    var dependencies = deps+' dependenc'+(deps!==1? 'ies':'y');
+    var license = pkg.license, version = pkg.version, updated = moment(mod).fromNow();
+    return {install, dependencies, license, version, updated};
   });
-  req.end();
+};
+
+function fitWidth(org, ins, upd) {
+  var iw = 110+(12+ins.length)*12*0.63, uw = 240+upd.length*11*0.63;
+  return Math.round(Math.max(org, iw, uw));
 };
 
 function svg(arg, pkg) {
   var a = arg, p = pkg, z = CARD;
-  z = z.replace(/{{a.width([\+\-\d]+)}}/g, (m, p1) => `${a.width+(parseFloat(p1)||0)}`);
-  z = z.replace(/{{a.height([\+\-\d]+)}}/g, (m, p1) => `${a.height+(parseFloat(p1)||0)}`);
-  z = z.replace(/{{a.margin([\+\-\d]+)}}/g, (m, p1) => `${a.margin+(parseFloat(p1)||0)}`);
+  a.width = fitWidth(a.width, p.install, p.updated);
+  z = z.replace(/{{a.width([\+\-\d]+)?}}/g, (m, p1) => `${a.width+(parseFloat(p1)||0)}`);
+  z = z.replace(/{{a.height([\+\-\d]+)?}}/g, (m, p1) => `${a.height+(parseFloat(p1)||0)}`);
+  z = z.replace(/{{a.margin([\+\-\d]+)?}}/g, (m, p1) => `${a.margin+(parseFloat(p1)||0)}`);
   z = z.replace(/{{p.install}}/g, p.install);
   z = z.replace(/{{p.dependencies}}/g, p.dependencies);
-  z = z.replace(/{{p.dependents}}/g, p.dependents);
+  z = z.replace(/{{p.license}}/g, p.license);
   z = z.replace(/{{p.version}}/g, p.version);
   z = z.replace(/{{p.updated}}/g, p.updated);
   return z.replace(/{{n.logo}}/g, LOGO);
 };
 
-
 var server = http.createServer((req, res) => {
-  var u = url.parse(req.url);
-  console.log(u);
-  res.end('Hello World');
+  console.log(`:> ${req.method} ${req.url}`);
+  var {path, search} = url.parse(req.url.toLowerCase());
+  var query = queryString.parse(search);
+  if(!path.endsWith('.svg')) return res.end();
+  var name = path.substring(1, path.length-4);
+  package(name).then((pkg) => {
+    res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+    res.end(svg(argument(query), pkg));
+  });
 });
 server.listen(E.PORT||80);
